59d6e0b3996b20ffbba3882e3b51a91a34ce787d
Pau Salanova Blanco <pauete.sb@gmail.com>
2022-07-12 23:22:56 +0200

    added some global methods for the level manager

--- src/main/java/com/fadedbytes/beospigotclient/BeoSpigotClient.java
+++ src/main/java/com/fadedbytes/beospigotclient/BeoSpigotClient.java
@@ -1,17 +1,23 @@
 package com.fadedbytes.beospigotclient;
 
-import org.bukkit.entity.Player;
+import com.fadedbytes.beospigotclient.level.LevelManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BeoSpigotClient extends JavaPlugin {
 
+    private static LevelManager levelManager;
+
     @Override
     public void onEnable() {
-
+        levelManager = new LevelManager();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static LevelManager getLevelManager() {
+        return levelManager;
     }
 }
