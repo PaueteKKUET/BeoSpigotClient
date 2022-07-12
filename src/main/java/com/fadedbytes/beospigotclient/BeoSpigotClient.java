4c687266b33dcf67e326ddea41f3774d1c5a34df
Pau Salanova Blanco <pauete.sb@gmail.com>
2022-07-12 23:02:27 +0200

    Initial commit

---
+++ src/main/java/com/fadedbytes/beospigotclient/BeoSpigotClient.java
@@ -0,0 +1,17 @@
+package com.fadedbytes.beospigotclient;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BeoSpigotClient extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
